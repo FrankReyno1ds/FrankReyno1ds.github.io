32622725ae82b93d5f3e240a33037e315d392c09
FrankReynolds <brooksbaker32@gmail.com>
2021-03-27 18:20:05 -0500

    first commit

---
+++ index-Sunny.js
@@ -0,0 +1,50 @@
+$(document).ready (function() {
+    var questionList = [{
+        'question' : 'How is a proper Milk Steak prepared',
+    
+        'answers' : ['Boiled over hard with the finest jelly beans, raw', 'Pan seared then milk basted with toasted jelly beans',
+    
+        'Grilled at 375 degrees for 5 minutes total flipping each minute topped with chilled milk and toasted jelly beans'],
+
+        'correctAnswer' : 'Boiled over hard with the finest jelly beans, raw'
+    
+    },
+    {
+        'question' : 'What are kitten mittens',
+
+        'answers' : ['Adoreable kitty kat paw themed oven mits', 'Hulk hands stylized like cat paws', 'Mittens for your cat so they wont be so loud'],
+
+        'correctAnswer' : 'Mittens for your cat so they wont be so loud'
+
+    },
+    {
+        'question' : 'Is fight milk the only dairy based alcoholic protien drink by body guards for body guards',
+
+        'answers' : ['yes', 'no'],
+
+        'correctAnswer' : 'yes'
+    },
+    {
+        'question' : 'Who is dayman',
+
+        'answers' : ['Wade boggs', 'The figher of the nightman', 'Ted cruz', 'Man who shushes'],
+
+        'correctAnswer' : 'The fighter of the nightman'
+    },
+    {
+        'question' : 'Is Its always sunny in Philidelphia the longest running live action comedy sitcom',
+
+        'answers' : ['yes', 'no'],
+
+        'correctAnswer' : 'yes'
+    },
+    {
+        'question' : 'What is Franks role in the gang',
+
+        'answers' : ['Money man', 'The brains', 'The dumb pretty one', 'Janitor'],
+
+        'correctAnswer' : 'Money man'
+    }]
+})
+
+var
